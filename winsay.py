a25cd1d1deb83b23ef94dd316430e2b71809c1b4
sukhbinder <ssingh@email.ccom>
2020-05-04 18:02:15 -0200

    Added ability to read text files with -i command

--- winsay.py
+++ winsay.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-import time
+
 import argparse
 import datetime
+import time
+
 import win32com.client as wincl
-
-
 
 
 def say(text):
@@ -14,19 +14,25 @@ def say(text):
     except Exception as ex:
         print("Error in speaking: ".format(ex.msg))
 
+
 def main():
     """
     Say in windows
     """
     parser = argparse.ArgumentParser(description="Say in windows")
-    parser.add_argument("text" ,type=str, nargs="*", help="sentence to speak", default="")
+    parser.add_argument("text", type=str, nargs="*",
+                        help="sentence to speak", default="")
+    parser.add_argument("-i", "--input", type=str,
+                        help="Text File to speak", default=None)
+
     args = parser.parse_args()
+    if args.input:
+        content = open(args.input).read()
+        say(content)
+
     if args.text:
         sentence = " ".join(args.text)
         say(sentence)
-    
-
-
 
 
 if __name__ == "__main__":
