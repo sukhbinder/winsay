fda26c65903837bfde20b9bb23fc1dc161096f47
Sukhbinder Singh <1002094@QuEST-RR01.COM>
2021-01-10 09:39:03 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # winsay
 Say command in windows
 
-![](img/youtube.png)
+```
+say -h
+usage: say [-h] [-i INPUT] [text [text ...]]
 
+Say in windows
+
+positional arguments:
+  text                  sentence to speak
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Text File to speak
+```
+
+
+### Back Story
+
+Read the backstory for this project [here](https://sukhbinder.wordpress.com/2020/09/30/winsay/)
 
 # Install
 pip install winsay
@@ -10,5 +27,6 @@ pip install winsay
 
 # Demo
 
+[![Check a demo](https://raw.githubusercontent.com/sukhbinder/winsay/master/img/youtube.png)](https://youtu.be/dZaV5MtG3n4)
 
-[![Check a demo](img/youtube.png)](https://youtu.be/dZaV5MtG3n4)
+
--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
+import pathlib
 from setuptools import find_packages, setup
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="winsay",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     license="Private",
     description="say in windows",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="sukhbinder",
     author_email="sukh2010@yahoo.com",
+    url = 'https://github.com/sukhbinder/winsay',
+    keywords = ["say", "windows", "mac", "computer", "speak",],
     entry_points={
         'console_scripts': ['say = winsay.winsay:main', ],
     },
     install_requires=["pywin32"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
 
 )
